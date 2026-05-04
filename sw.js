35acd4f082a32a05c72ec9e953bf0cd1f9174aab
Saini Pustak Bhandar <sainipustakbhandarchh@gmail.com>
2026-05-03 19:09:36 -0500

    Update favicon to favicon.ico, add Service Worker for PWA support, and update manifest icons

---
+++ sw.js
@@ -0,0 +1,36 @@
+const CACHE_NAME = 'saurabh-saini-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './logo_v3.png',
+  './favicon.ico',
+  './apple-touch-icon-new.png',
+  './Profile Picture.png',
+  './manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(ASSETS))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
